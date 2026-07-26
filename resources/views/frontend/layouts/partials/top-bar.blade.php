1210ce8d00a9e703be47ecb67b6476f33ea7cd81
Shuvo <shuvot01794@gmail.com>
2026-07-26 21:22:20 +0600

    feat(frontend): add custom topbar views for auth and guest users

--- resources/views/frontend/layouts/partials/top-bar.blade.php
+++ resources/views/frontend/layouts/partials/top-bar.blade.php
@@ -6,10 +6,15 @@
                 <span class="ms-3"><i class="bi bi-envelope"></i> support@freshmart.com</span>
             </div>
             <div class="col-md-6 text-end">
-                <span class="ms-3"><i class="bi bi-box-arrow-in-right"></i>
-                    <a href="{{ route('login') }}" class="text-white text-decoration-none">Login</a></span>
-                <span class="ms-3"><i class="bi bi-person"></i>
-                    <a href="{{ route('register') }}" class="text-white text-decoration-none">Register</a></span>
+                @auth('web')
+                    <span class="ms-3"><i class="bi bi-house-gear-fill"></i>
+                        <a href="{{ route('dashboard') }}" class="text-white text-decoration-none">Dashboard</a></span>
+                @else
+                    <span class="ms-3"><i class="bi bi-box-arrow-in-right"></i>
+                        <a href="{{ route('login') }}" class="text-white text-decoration-none">Login</a></span>
+                    <span class="ms-3"><i class="bi bi-person"></i>
+                        <a href="{{ route('register') }}" class="text-white text-decoration-none">Register</a></span>
+                @endauth
             </div>
         </div>
     </div>
